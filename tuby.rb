160d6fa9069df273707b97fc8dd59f9a8e7a1ff8
Roy Wei <roywjy@gmail.com>
2016-12-15 18:57:43 +0800

    socket handling

--- tuby.rb
+++ tuby.rb
@@ -2,10 +2,26 @@ require "socket"
 
 class Tuby
   def initialize(port)
-    @server = TCPserver.new(port)
+    @server = TCPServer.new(port)
   end
 
   def start
-    socket = @server.accept
+    loop do
+      socket = @server.accept
+
+      data = socket.readpartial(1024)
+      puts data
+
+      socket.write "HTTP/ 1.1 200 OK\r\n"
+      socket.write "\r\n"
+      socket.write "hello\n"
+
+      socket.close
+    end
   end
 end
+
+
+server = Tuby.new(3005)
+puts "Plugging Tuby into port 3005"
+server.start
